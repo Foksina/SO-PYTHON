68ac082c12f79bbf5616f8fe6aa455b8e2ae03e5
Foksina <106610411+Foksina@users.noreply.github.com>
2024-05-20 09:59:02 +0200

    Update studentB.py

--- studentB.py
+++ studentB.py
@@ -1,8 +1,55 @@
+# 1 - użytkownik
+# 2 - PC
+import random
 def ai_move(board):
-    pass
+    x_pc_move = random.randint(0,5)
+    y_pc_move = random.randint(0,5)
+
+    while not board.markSpot(False, x_pc_move, y_pc_move):
+        x_pc_move = random.randint(0,5)
+        y_pc_move = random.randint(0,5)
+
+    board.markSpot(False, x_pc_move, y_pc_move)
+    return board
 
 def get_user_move(board):
-    pass
+    print("Your turn!")
+    while True:
+        x_move = input("X: ")
+        if(x_move >=0 and x_move <5):
+            break
+        else:
+            print("x should be between 0 and 5")
+    
+    while True:
+        y_move = input("Y: ")
+        if(y_move >=0 and y_move <5):
+            break
+        else:
+            print("y should be between 0 and 5")
+
+    print("This position is not empty!")
+
+    while board.markSpot(True, x_move, y_move) == False:
+        print("Try again!")
+        while True:
+            x_move = input("X: ")
+            if(x_move >=0 and x_move <5):
+                break
+            else:
+                print("x should be between 0 and 5")
+    
+        while True:
+            y_move = input("Y: ")
+            if(y_move >=0 and y_move <5):
+                break
+            else:
+                print("y should be between 0 and 5")
+
+        print("This position is not empty!")    
+    
+    board.markSpot(True, x_move, y_move)
+    return board
 
 def is_player_starting():
     print("Do you want to start? T/F")
